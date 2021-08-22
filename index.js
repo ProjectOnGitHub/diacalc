18f09f2f92b63c6c7e882144fa0bcc7fa09a6e1d
Aleksandr <soft@vhlv.ru>
2021-08-22 19:57:44 +0300

    сделал валидацию на js, но пока только для одного поля

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ const formElement = document.querySelector('.form');
 const calcResult = formElement.querySelector('.form__result');
 const inputCarbs = formElement.querySelector('.form__input-carbs');
 const inputUnit = formElement.querySelector('.form__input-unit');
+const formInput = formElement.querySelector('.form__input');
+const formError = formElement.querySelector(`.${formInput.id}-error`);
 
 formElement.addEventListener('submit', formSubmitHandler);
 
 
 function calcCarbs(carbs, unit) {
-  carbs = inputCarbs.value;
-  unit = inputUnit.value;
+  carbs = inputCarbs.value.replace(/,/, '.');
+  unit = inputUnit.value.replace(/,/, '.');
   const carbsSum = (100 * 10 / carbs * unit).toFixed(1);
   return calcResult.textContent = carbsSum;
 }
@@ -25,3 +27,31 @@ function formSubmitHandler(e) {
   e.preventDefault();
   calcCarbs();
 }
+
+const showInputError = (element, errorMessage) => {
+  element.classList.add('form__input_type_error');
+  formError.textContent = errorMessage;
+  formError.classList.add('form__input-error_active');
+};
+
+const hideInputError = (element) => {
+  element.classList.remove('form__input_type_error');
+  formError.classList.remove('form__input-error_active');
+  formError.textContent = '';
+};
+
+const isValid = () => {
+  if (!formInput.validity.valid) {
+    showInputError(formInput, formInput.validationMessage);
+  } else {
+    hideInputError(formInput);
+  }
+};
+
+formElement.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+});
+
+formInput.addEventListener('input', isValid);
+
+
