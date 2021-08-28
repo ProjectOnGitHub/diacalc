92d93f869930c23f488207c5854d7662fd3daabf
Aleksandr <soft@vhlv.ru>
2021-08-28 16:11:13 +0300

    обновил ридми, убрал неиспользуемые переменные

--- index.js
+++ index.js
@@ -1,48 +1,34 @@
-/*
-Нужна форма с двумя полями.
-В одном поле вводится количество углеводов на 100 грамм.
-В другое поле - количество хлебных единиц.
-При нажатии на кнопку все считается
-
-*/
 const formElement = document.querySelector('.form');
 const calcResult = formElement.querySelector('.form__result');
 const inputCarbs = formElement.querySelector('.form__input-carbs');
 const inputUnit = formElement.querySelector('.form__input-unit');
-const formInput = formElement.querySelector('.form__input');
-const formError = formElement.querySelector(`.${formInput.id}-error`);
 
-formElement.addEventListener('submit', formSubmitHandler);
-
-
-function calcCarbs(carbs, unit) {
-  carbs = inputCarbs.value.replace(/,/, '.');
-  unit = inputUnit.value.replace(/,/, '.');
-  const carbsSum = (100 * 10 / carbs * unit).toFixed(1);
-  return calcResult.textContent = carbsSum;
+function calcCarbs() {
+  const carbs = inputCarbs.value.replace(/,/, '.');
+  const unit = inputUnit.value.replace(/,/, '.');
+  const carbsSum = (100 * 10) / (carbs * unit).toFixed(1);
+  calcResult.textContent = carbsSum;
+  return carbsSum;
 }
-
 
 function formSubmitHandler(e) {
   e.preventDefault();
   calcCarbs();
 }
 
+formElement.addEventListener('submit', formSubmitHandler);
+
 /* Валидация формы */
 
 const showInputError = (formElement, inputElement, errorMessage) => {
-  // Находим элемент ошибки внутри самой функции
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  // Остальной код такой же
   inputElement.classList.add('form__input_type_error');
   errorElement.textContent = errorMessage;
   errorElement.classList.add('form__input-error_active');
 };
 
 const hideInputError = (formElement, inputElement) => {
-  // Находим элемент ошибки
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  // Остальной код такой же
   inputElement.classList.remove('form__input_type_error');
   errorElement.classList.remove('form__input-error_active');
   errorElement.textContent = '';
@@ -50,85 +36,42 @@ const hideInputError = (formElement, inputElement) => {
 
 const isValid = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
-    // showInputError теперь получает параметром форму, в которой
-    // находится проверяемое поле, и само это поле
     showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
-    // hideInputError теперь получает параметром форму, в которой
-    // находится проверяемое поле, и само это поле
     hideInputError(formElement, inputElement);
   }
 };
 
-
-// Функция принимает массив полей
-
-const hasInvalidInput = (inputList) => {
-  // проходим по этому массиву методом some
-  return inputList.some((inputElement) => {
-    // Если поле не валидно, колбэк вернёт true
-    // Обход массива прекратится и вся фунцкция
-    // hasInvalidInput вернёт true
-
-    return !inputElement.validity.valid;
-  })
-};
-
-// Функция принимает массив полей ввода
-// и элемент кнопки, состояние которой нужно менять
+const hasInvalidInput = (inputList) => inputList.some((inputElement) => !inputElement.validity.valid);
 
 const toggleButtonState = (inputList, buttonElement) => {
-  // Если есть хотя бы один невалидный инпут
   if (hasInvalidInput(inputList)) {
-    // сделай кнопку неактивной
     buttonElement.classList.add('form__button_inactive');
   } else {
-    // иначе сделай кнопку активной
     buttonElement.classList.remove('form__button_inactive');
   }
 };
 
 const setEventListeners = (formElement) => {
-  // Находим все поля внутри формы,
-  // сделаем из них массив методом Array.from
   const inputList = Array.from(formElement.querySelectorAll('.form__input'));
-
-  // Найдём в текущей форме кнопку отправки
   const buttonElement = formElement.querySelector('.form__button');
   toggleButtonState(inputList, buttonElement);
-
-  // Обойдём все элементы полученной коллекции
   inputList.forEach((inputElement) => {
-    // каждому полю добавим обработчик события input
     inputElement.addEventListener('input', () => {
-      // Внутри колбэка вызовем isValid,
-      // передав ей форму и проверяемый элемент
       isValid(formElement, inputElement);
-      // Вызовем toggleButtonState и передадим ей массив полей и кнопку
       toggleButtonState(inputList, buttonElement);
     });
   });
 };
 
 const enableValidation = () => {
-  // Найдём все формы с указанным классом в DOM,
-  // сделаем из них массив методом Array.from
   const formList = Array.from(document.querySelectorAll('.form'));
-
-  // Переберём полученную коллекцию
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', (evt) => {
-      // У каждой формы отменим стандартное поведение
       evt.preventDefault();
     });
-
-    // Для каждой формы вызовем функцию setEventListeners,
-    // передав ей элемент формы
     setEventListeners(formElement);
   });
 };
 
-// Вызовем функцию
 enableValidation();
-
-
